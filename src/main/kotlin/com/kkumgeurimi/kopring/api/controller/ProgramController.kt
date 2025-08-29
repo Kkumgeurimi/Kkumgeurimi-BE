8beda2cd4382f82e5401db35131c8f13cf93d91c
judith <8w8u8@naver.com>
2025-08-29 21:50:36 +0900

    fix: 스웨거 어노테이션 누락

--- src/main/kotlin/com/kkumgeurimi/kopring/api/controller/ProgramController.kt
+++ src/main/kotlin/com/kkumgeurimi/kopring/api/controller/ProgramController.kt
@@ -1,6 +1,8 @@
 package com.kkumgeurimi.kopring.api.controller
 
 import com.kkumgeurimi.kopring.domain.program.service.ProgramService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 
 @Tag(name = "Program")
