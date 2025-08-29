29e51f53daa74cd911109575e396d40505eb7710
judith <8w8u8@naver.com>
2025-08-30 06:03:01 +0900

    feat: 학생 관심사 설정

---
+++ src/main/kotlin/com/kkumgeurimi/kopring/api/dto/InterestCategoryRequest.kt
@@ -0,0 +1,8 @@
+package com.kkumgeurimi.kopring.api.dto
+
+import jakarta.validation.constraints.NotNull
+
+data class InterestCategoryRequest(
+    @field:NotNull(message = "관심사 카테고리 ID는 필수입니다")
+    val interestCategoryId: Int
+)
