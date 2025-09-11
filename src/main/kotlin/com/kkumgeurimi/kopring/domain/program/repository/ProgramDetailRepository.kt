54e17b8a9ea8f4f40ae7b1c571c3b1977ade8697
Seohyun Lee <applemint2003@gmail.com>
2025-09-11 14:51:39 +0900

    refactor: Swagger에서 서버 변경 용이성 추가, 사용하지 않는 임포트 제거

--- src/main/kotlin/com/kkumgeurimi/kopring/domain/program/repository/ProgramDetailRepository.kt
+++ src/main/kotlin/com/kkumgeurimi/kopring/domain/program/repository/ProgramDetailRepository.kt
@@ -1,8 +1,6 @@
 package com.kkumgeurimi.kopring.domain.program.repository
 
-import com.kkumgeurimi.kopring.domain.program.entity.Program
 import com.kkumgeurimi.kopring.domain.program.entity.ProgramLike
-import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ProgramDetailRepository : JpaRepository<ProgramLike, Long> {
