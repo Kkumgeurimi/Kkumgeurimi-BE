1cac9d650efb78b788e976995d1b0f42b29ca0bd
judith <8w8u8@naver.com>
2025-09-19 08:58:07 +0900

    fix: MyProgramWithRegistrationStatusResponse 반환요소 기획에 맞게 수정

--- src/main/kotlin/com/kkumgeurimi/kopring/api/dto/program/MyProgramWithRegistrationStatusResponse.kt
+++ src/main/kotlin/com/kkumgeurimi/kopring/api/dto/program/MyProgramWithRegistrationStatusResponse.kt
@@ -1,8 +1,6 @@
 package com.kkumgeurimi.kopring.api.dto.program
 
-import com.kkumgeurimi.kopring.domain.common.CostType
 import com.kkumgeurimi.kopring.domain.common.InterestCategory
-import com.kkumgeurimi.kopring.domain.common.ProgramType
 import com.kkumgeurimi.kopring.domain.program.entity.ProgramRegistration
 import com.kkumgeurimi.kopring.domain.program.entity.RegistrationStatus
 import java.time.LocalDate
@@ -12,12 +10,9 @@ data class MyProgramWithRegistrationStatusResponse(
     val programId: String,
     val programTitle: String,
     val provider: String?,
-    val programTypeLabel: String?,
     val startDate: LocalDate?,
     val endDate: LocalDate?,
-    val costType: CostType?,
     val imageUrl: String?,
-    val venueRegion: String?,
     val interestCategoryLabel: String?,
     val registrationStatus: RegistrationStatus?,
 
@@ -35,20 +30,13 @@ data class MyProgramWithRegistrationStatusResponse(
                 InterestCategory.fromCode(it).label
             }
 
-            val programTypeLabel = program.programType.let {
-                ProgramType.fromCode(it)?.label
-            }
-
             return MyProgramWithRegistrationStatusResponse(
                 programId = program.programId,
                 programTitle = program.programTitle,
                 provider = program.provider,
-                programTypeLabel = programTypeLabel,
                 startDate = program.startDate,
                 endDate = program.endDate,
-                costType = program.costType,
                 imageUrl = program.imageUrl,
-                venueRegion = program.venueRegion,
                 interestCategoryLabel = interestCategoryLabel,
                 registrationStatus = registration.registrationStatus,
                 isReviewed = !registration.reviewScore.isNullOrBlank() || !registration.reviewMessage.isNullOrBlank(),
